3bc69d9c365617fc83053a41fc295bad2ae65edf
AshwiniKumar Meshram <ashwinimeshram@gmail.com>
2026-04-20 09:30:55 +0530

    Cookies Set

--- app.py
+++ app.py
@@ -27,6 +27,11 @@ app.config["MAIL_USERNAME"] = "info@nagpurrealtyhub.com"
 app.config["MAIL_PASSWORD"] = "your_app_password"
 app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "uploads")
 
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_COOKIE_SECURE"] = True
+app.config["JWT_COOKIE_SAMESITE"] = "None"
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+
 # INIT EXTENSIONS
 mail.init_app(app)
 db.init_app(app)
